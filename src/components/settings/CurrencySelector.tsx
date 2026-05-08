39d6e6ecb5971b1353c7e38b6c0a663e79f3856b
mucaca1 <matejkrc1@gmail.com>
2026-05-08 22:56:59 +0200

    `Update CurrencySelector component to use MUI Select component`

--- src/components/settings/CurrencySelector.tsx
+++ src/components/settings/CurrencySelector.tsx
@@ -1,30 +1,30 @@
-import React from "react";
-import { FormControl, InputLabel, Select, MenuItem } from "@mui/material";
+import { MenuItem, OutlinedInput, Select } from "@mui/material";
 import { useTranslation } from "react-i18next";
 import { CURRENCIES } from "../../types/price";
 
-interface CurrencySelectorProps {
+export function CurrencySelector({
+    value,
+    onChange,
+}: {
     value: string;
     onChange: (currency: string) => void;
-}
-
-export function CurrencySelector({ value, onChange }: CurrencySelectorProps) {
+}) {
     const { t } = useTranslation();
 
     return (
-        <FormControl fullWidth size="small">
-            <InputLabel>{t("settings.general.currency")}</InputLabel>
-            <Select
-                value={value}
-                onChange={(e) => onChange(e.target.value)}
-                label={t("settings.general.currency")}
-            >
-                {CURRENCIES.map((currency) => (
-                    <MenuItem key={currency.value} value={currency.value}>
-                        {currency.label}
-                    </MenuItem>
-                ))}
-            </Select>
-        </FormControl>
+        <Select
+            value={value}
+            onChange={(e) => onChange(e.target.value)}
+            input={<OutlinedInput />}
+            size="small"
+            sx={{ maxWidth: 220 }}
+            fullWidth
+        >
+            {CURRENCIES.map((currency) => (
+                <MenuItem key={currency.value} value={currency.value}>
+                    {currency.label}
+                </MenuItem>
+            ))}
+        </Select>
     );
 }
